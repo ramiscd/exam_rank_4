ad9930446fe5b971506fe26bf1f88d2c29e7f289
Ramis Cesar Damasceno <rdamasce@c1r9p1.42sp.org.br>
2026-08-17 21:03:10 -0300

    wip: learning picoshell

--- picoshell/picoshell.c
+++ picoshell/picoshell.c
@@ -3,21 +3,37 @@
 #include <sys/types.h>
 #include <string.h>
 
-int main(int ac, char **av, char **envp)
-{
-	pid_t pid = fork();
+// fork()
+// pipe()
+// dup2()
+// execve()
+// close()
+// wait()
 
-	if(pid == 0)
-	{
-		printf("sou filho\n");
-		// execve("ls");
-		for(int i = 0; i <= strlen(*envp); i++)
-		{
-			printf("%s\n", *envp);
-		}
-	}
-	else
-	{
-		printf("Sou pai\n");
-	}
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int pipefd[2];
+    int backup;
+    char buffer[100];
+
+    pipe(pipefd);
+
+    backup = dup(STDOUT_FILENO);
+
+    dup2(pipefd[1], STDOUT_FILENO);
+
+    printf("hello\n");
+
+    fflush(stdout);
+
+    dup2(backup, STDOUT_FILENO);
+
+    int n = read(pipefd[0], buffer, 100);
+
+    buffer[n] = '\0';
+
+    printf("Recebi: %s", buffer);
 }
