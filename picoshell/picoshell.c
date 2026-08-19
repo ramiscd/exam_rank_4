64922468ab4dc428abe704576236c07aa3fefc51
Ramis Cesar Damasceno <rdamasce@c1r3p1.42sp.org.br>
2026-08-19 20:45:01 -0300

    wip: entendendo pipes

--- picoshell/picoshell.c
+++ picoshell/picoshell.c
@@ -2,38 +2,36 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <string.h>
-
-// fork()
-// pipe()
-// dup2()
-// execve()
-// close()
-// wait()
-
 #include <unistd.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int pipefd[2];
-    int backup;
-    char buffer[100];
+	int pipefd[2]; // Criar um array com 2 valores que serao o stdin e stdout
+	char buffer[100]; // vai guardar o valor que queremos transmitir
 
-    pipe(pipefd);
+	pipe(pipefd);
 
-    backup = dup(STDOUT_FILENO);
+	pid_t pid = fork(); // Cria um processo filho a partir do pai, agora temos 2 programas em paralelo.
 
-    dup2(pipefd[1], STDOUT_FILENO);
+	if(pid == 0) // processo filho	o pid e 0
+	{
+		close(pipefd[1]);
+		//  read(DE_ONDE_LER, ONDE_COLOCAR_OS_DADOS, QUANTOS_BYTES_LER);
+		read(pipefd[0], buffer, 100); // ler o que vem da saida do cano e colocar no buffer
 
-    printf("hello\n");
+		printf("Valor que saiu do cano: %s", buffer);
+	}
 
-    fflush(stdout);
+	else // processo pai
+	{
+		close(pipefd[0]);
+	
+		write(pipefd[1], "hello", 5); // Escreva o hello na entrada no cano (PIPE)
 
-    dup2(backup, STDOUT_FILENO);
-
-    int n = read(pipefd[0], buffer, 100);
-
-    buffer[n] = '\0';
-
-    printf("Recebi: %s", buffer);
+		close(pipefd[0]);
+	}
 }
+
+// Ou seja, o pai possui o "hello" e escreve no pipe.
+// O filho possui um buffer vazio e lê o pipe.
